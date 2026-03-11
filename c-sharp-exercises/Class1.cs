0b2e674f9e6bff2f0c8a3c677b7f1793a15c279c
Sam <samuelgdukes@gmail.com>
2026-03-11 12:05:02 +0000

    Save Class

--- c-sharp-exercises/Class1.cs
+++ c-sharp-exercises/Class1.cs
@@ -2,5 +2,15 @@
 {
     internal class PracticeClass
     {
+        class Book
+        {
+            string Title { get; set; }
+            int Pages {  get; set; }
+
+            public Book(string name)
+            {
+                Title = name;
             }
+        }       
+    }
 }
